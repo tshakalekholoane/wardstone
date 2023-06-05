64caa82e17a627a2b3952964fb978ba122c3ef92
Tshaka Lekholoane <mail+git@tshaka.dev>
2023-06-05 15:49:05 +0200

    feat: recommend alternatives for invalid primitives

--- src/primitives/hash.rs
+++ src/primitives/hash.rs
@@ -1,37 +1,108 @@
-/// cbindgen:field-names=[digest_len]
 #[repr(C)]
-#[derive(PartialEq, Eq, Clone, Copy)]
-pub struct Hash(pub u16);
+#[derive(PartialEq, Eq, Clone, Copy, Debug)]
+pub struct Hash {
+  pub name: &'static str,
+  pub digest_len: u16,
+}
 
 #[no_mangle]
-pub static BLAKE2b_256: Hash = Hash(256);
+pub static BLAKE2b_256: Hash = Hash {
+  name: "BLAKE2b_256",
+  digest_len: 256,
+};
+
 #[no_mangle]
-pub static BLAKE2b_384: Hash = Hash(384);
+pub static BLAKE2b_384: Hash = Hash {
+  name: "BLAKE2b_384",
+  digest_len: 384,
+};
+
 #[no_mangle]
-pub static BLAKE2b_512: Hash = Hash(512);
+pub static BLAKE2b_512: Hash = Hash {
+  name: "BLAKE2b_512",
+  digest_len: 512,
+};
+
 #[no_mangle]
-pub static BLAKE2s_256: Hash = Hash(256);
+pub static BLAKE2s_256: Hash = Hash {
+  name: "BLAKE2s_256",
+  digest_len: 256,
+};
+
 #[no_mangle]
-pub static MD4: Hash = Hash(128);
+pub static BLAKE3_256: Hash = Hash {
+  name: "BLAKE3_256",
+  digest_len: 256,
+};
+
 #[no_mangle]
-pub static MD5: Hash = Hash(128);
+pub static MD4: Hash = Hash {
+  name: "MD4",
+  digest_len: 128,
+};
+
 #[no_mangle]
-pub static RIPEMD160: Hash = Hash(160);
+pub static MD5: Hash = Hash {
+  name: "MD5",
+  digest_len: 128,
+};
+
 #[no_mangle]
-pub static SHA1: Hash = Hash(160);
+pub static RIPEMD160: Hash = Hash {
+  name: "RIPEMD160",
+  digest_len: 160,
+};
+
 #[no_mangle]
-pub static SHA256: Hash = Hash(256);
+pub static SHA1: Hash = Hash {
+  name: "SHA1",
+  digest_len: 160,
+};
+
 #[no_mangle]
-pub static SHA3_224: Hash = Hash(224);
+pub static SHA256: Hash = Hash {
+  name: "SHA256",
+  digest_len: 256,
+};
+
 #[no_mangle]
-pub static SHA3_256: Hash = Hash(256);
+pub static SHA3_224: Hash = Hash {
+  name: "SHA3_224",
+  digest_len: 224,
+};
+
 #[no_mangle]
-pub static SHA3_384: Hash = Hash(384);
+pub static SHA3_256: Hash = Hash {
+  name: "SHA3_256",
+  digest_len: 256,
+};
+
 #[no_mangle]
-pub static SHA3_512: Hash = Hash(512);
+pub static SHA3_384: Hash = Hash {
+  name: "SHA3_384",
+  digest_len: 384,
+};
+
 #[no_mangle]
-pub static SHA3_512_224: Hash = Hash(224);
+pub static SHA3_512: Hash = Hash {
+  name: "SHA3_512",
+  digest_len: 512,
+};
+
 #[no_mangle]
-pub static SHA3_512_256: Hash = Hash(256);
+pub static SHA3_512_224: Hash = Hash {
+  name: "SHA3_512_224",
+  digest_len: 224,
+};
+
 #[no_mangle]
-pub static SHA512: Hash = Hash(512);
+pub static SHA3_512_256: Hash = Hash {
+  name: "SHA3_512_256",
+  digest_len: 256,
+};
+
+#[no_mangle]
+pub static SHA512: Hash = Hash {
+  name: "SHA512",
+  digest_len: 512,
+};
--- src/standards/lenstra.rs
+++ src/standards/lenstra.rs
@@ -12,23 +12,22 @@ pub enum ValidationError {
 type Result<T> = result::Result<T, ValidationError>;
 
 const BASE_YEAR: u16 = 1982;
-const BASE_SECURITY_LEVEL: u16 = 56;
+const BASE_SECURITY: u16 = 56;
 
-fn calculate_year(security_level: u16) -> Result<u16> {
-  if security_level < BASE_SECURITY_LEVEL {
+fn calculate_year(security: u16) -> Result<u16> {
+  if security < BASE_SECURITY {
     return Err(ValidationError::SecurityLevelTooLow);
   }
-  Ok(BASE_YEAR + (((security_level + (security_level << 1)) - 168) >> 1))
+  Ok(BASE_YEAR + (((security + (security << 1)) - 168) >> 1))
 }
 
 pub fn validate_hash(hash: &Hash, expiry: u16) -> Result<bool> {
-  let security_level = hash.0 >> 1;
-  calculate_year(security_level).map(|year| year >= expiry)
+  let security = hash.digest_len >> 1;
+  calculate_year(security).map(|year| year >= expiry)
 }
 
 pub fn validate_symmetric(symmetric: &Symmetric, expiry: u16) -> Result<bool> {
-  let security_level = symmetric.0;
-  calculate_year(security_level).map(|year| year >= expiry)
+  calculate_year(symmetric.security).map(|year| year >= expiry)
 }
 
 #[no_mangle]
