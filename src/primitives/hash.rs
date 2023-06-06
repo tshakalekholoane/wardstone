c61a4e62041e314b9445f1fc8962d37a42d751c1
Tshaka Lekholoane <mail+git@tshaka.dev>
2023-06-06 12:56:38 +0200

    docs: document primitives

--- src/primitives/hash.rs
+++ src/primitives/hash.rs
@@ -5,119 +5,192 @@ pub struct Hash {
   pub digest_len: u16,
 }
 
+/// The BLAKE2b hash function as defined by [RFC 7693].
+///
+/// [RFC 7693]: https://www.rfc-editor.org/rfc/rfc7693.html
 #[no_mangle]
 pub static BLAKE2b_256: Hash = Hash {
   id: 1,
   digest_len: 256,
 };
 
+/// The BLAKE2b hash function as defined by [RFC 7693].
+///
+/// [RFC 7693]: https://www.rfc-editor.org/rfc/rfc7693.html
 #[no_mangle]
 pub static BLAKE2b_384: Hash = Hash {
   id: 2,
   digest_len: 384,
 };
 
+/// The BLAKE2b hash function as defined by [RFC 7693].
+///
+/// [RFC 7693]: https://www.rfc-editor.org/rfc/rfc7693.html
 #[no_mangle]
 pub static BLAKE2b_512: Hash = Hash {
   id: 3,
   digest_len: 512,
 };
 
+/// The BLAKE2s hash function as defined by [RFC 7693].
+///
+/// [RFC 7693]: https://www.rfc-editor.org/rfc/rfc7693.html
 #[no_mangle]
 pub static BLAKE2s_256: Hash = Hash {
   id: 4,
   digest_len: 256,
 };
 
+/// The MD4 hash function as defined in [RFC 1320].
+///
+/// **Warning:** This algorithm has been shown to be broken. It should
+/// only be used where compatibility with legacy systems, not security,
+/// is the goal.
+///
+/// [RFC 1320]: https://www.rfc-editor.org/rfc/rfc1320.html
 #[no_mangle]
 pub static MD4: Hash = Hash {
   id: 5,
   digest_len: 128,
 };
 
+/// The MD5 hash function as defined in [RFC 1321].
+///
+/// **Warning:** This algorithm has been shown to lack collision
+/// resistance and should generally not be used for secure applications.
+///
+/// [RFC 1321]: https://www.rfc-editor.org/rfc/rfc1321.html
 #[no_mangle]
 pub static MD5: Hash = Hash {
   id: 6,
   digest_len: 128,
 };
 
+/// The RIPEMD-160 hash function.
+///
+/// **Warning:** This algorithm has been shown to be broken. It should
+/// only be used where compatibility with legacy systems, not security,
+/// is the goal.
 #[no_mangle]
 pub static RIPEMD160: Hash = Hash {
   id: 7,
   digest_len: 160,
 };
 
+/// The SHA1 hash function as defined in [RFC 3174].
+///
+/// **Warning:** This algorithm has been shown to lack collision
+/// resistance and should generally not be used for secure applications.
+///
+/// [RFC 1321]: https://www.rfc-editor.org/rfc/rfc3174.html
 #[no_mangle]
 pub static SHA1: Hash = Hash {
   id: 8,
   digest_len: 160,
 };
 
+/// The SHA224 hash function as defined in [FIPS 180-4].
+///
+/// [FIPS 180-4]: https://doi.org/10.6028/NIST.FIPS.180-4
+#[no_mangle]
 pub static SHA224: Hash = Hash {
   id: 9,
   digest_len: 224,
 };
 
+/// The SHA256 hash function as defined in [FIPS 180-4].
+///
+/// [FIPS 180-4]: https://doi.org/10.6028/NIST.FIPS.180-4
 #[no_mangle]
 pub static SHA256: Hash = Hash {
   id: 10,
   digest_len: 256,
 };
 
+/// The SHA384 hash function as defined in [FIPS 180-4].
+///
+/// [FIPS 180-4]: https://doi.org/10.6028/NIST.FIPS.180-4
 #[no_mangle]
 pub static SHA384: Hash = Hash {
   id: 11,
   digest_len: 384,
 };
 
+/// The SHA3-224 hash function as defined in [FIPS 202].
+///
+/// [FIPS]: https://doi.org/10.6028/NIST.FIPS.202
 #[no_mangle]
 pub static SHA3_224: Hash = Hash {
   id: 12,
   digest_len: 224,
 };
 
+/// The SHA3-256 hash function as defined in [FIPS 202].
+///
+/// [FIPS]: https://doi.org/10.6028/NIST.FIPS.202
 #[no_mangle]
 pub static SHA3_256: Hash = Hash {
   id: 13,
   digest_len: 256,
 };
 
+/// The SHA3-384 hash function as defined in [FIPS 202].
+///
+/// [FIPS]: https://doi.org/10.6028/NIST.FIPS.202
 #[no_mangle]
 pub static SHA3_384: Hash = Hash {
   id: 14,
   digest_len: 384,
 };
 
+/// The SHA3-512 hash function as defined in [FIPS 202].
+///
+/// [FIPS]: https://doi.org/10.6028/NIST.FIPS.202
 #[no_mangle]
 pub static SHA3_512: Hash = Hash {
   id: 15,
   digest_len: 512,
 };
 
+/// The SHA3-512/224 hash function as defined in [FIPS 202].
+///
+/// [FIPS]: https://doi.org/10.6028/NIST.FIPS.202
 #[no_mangle]
 pub static SHA3_512_224: Hash = Hash {
   id: 16,
   digest_len: 224,
 };
 
+/// The SHA3-512/256 hash function as defined in [FIPS 202].
+///
+/// [FIPS]: https://doi.org/10.6028/NIST.FIPS.202
 #[no_mangle]
 pub static SHA3_512_256: Hash = Hash {
   id: 17,
   digest_len: 256,
 };
 
+/// The SHA512 hash function as defined in [FIPS 180-4].
+///
+/// [FIPS 180-4]: https://doi.org/10.6028/NIST.FIPS.180-4
 #[no_mangle]
 pub static SHA512: Hash = Hash {
   id: 18,
   digest_len: 512,
 };
 
+/// The SHA512/224 hash function as defined in [FIPS 180-4].
+///
+/// [FIPS 180-4]: https://doi.org/10.6028/NIST.FIPS.180-4
 #[no_mangle]
 pub static SHA512_224: Hash = Hash {
   id: 19,
   digest_len: 224,
 };
 
+/// The SHA512/256 hash function as defined in [FIPS 180-4].
+///
+/// [FIPS 180-4]: https://doi.org/10.6028/NIST.FIPS.180-4
 #[no_mangle]
 pub static SHA512_256: Hash = Hash {
   id: 20,
--- src/primitives/symmetric.rs
+++ src/primitives/symmetric.rs
@@ -5,24 +5,37 @@ pub struct Symmetric {
   pub security: u16,
 }
 
+/// The Advanced Encryption Standard algorithm as defined in [FIPS 197].
+///
+/// [FIPS 197]: https://doi.org/10.6028/NIST.FIPS.197
 #[no_mangle]
 pub static AES128: Symmetric = Symmetric {
   id: 1,
   security: 128,
 };
 
+/// The Advanced Encryption Standard algorithm as defined in [FIPS 197].
+///
+/// [FIPS 197]: https://doi.org/10.6028/NIST.FIPS.197
 #[no_mangle]
 pub static AES192: Symmetric = Symmetric {
   id: 2,
   security: 192,
 };
 
+/// The Advanced Encryption Standard algorithm as defined in [FIPS 197].
+///
+/// [FIPS 197]: https://doi.org/10.6028/NIST.FIPS.197
 #[no_mangle]
 pub static AES256: Symmetric = Symmetric {
   id: 3,
   security: 256,
 };
 
+/// The three-key Triple Data Encryption Algorithm as defined in
+/// [SP800-67].
+///
+/// [SP800-67]: https://doi.org/10.6028/NIST.SP.800-67r2
 #[no_mangle]
 pub static TDEA: Symmetric = Symmetric {
   id: 4,
