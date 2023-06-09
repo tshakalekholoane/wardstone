19ff58fd282e06fe3811cddef0533cf802d04b12
Tshaka Lekholoane <mail+git@tshaka.dev>
2023-06-09 14:37:22 +0200

    feat: add two-key Triple DES symmetric primitive

--- src/primitives/symmetric.rs
+++ src/primitives/symmetric.rs
@@ -32,12 +32,22 @@ pub static AES256: Symmetric = Symmetric {
   security: 256,
 };
 
+/// The two-key Triple Data Encryption Algorithm as defined in
+/// [SP800-67].
+///
+/// [SP800-67]: https://doi.org/10.6028/NIST.SP.800-67r2
+#[no_mangle]
+pub static TDEA2: Symmetric = Symmetric {
+  id: 4,
+  security: 95,
+};
+
 /// The three-key Triple Data Encryption Algorithm as defined in
 /// [SP800-67].
 ///
 /// [SP800-67]: https://doi.org/10.6028/NIST.SP.800-67r2
 #[no_mangle]
-pub static TDEA: Symmetric = Symmetric {
-  id: 4,
+pub static TDEA3: Symmetric = Symmetric {
+  id: 5,
   security: 112,
 };
