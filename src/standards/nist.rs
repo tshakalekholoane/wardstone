2d698cda42c6d478047acc0b9b0ff4ac50a02ae6
Tshaka Lekholoane <mail+git@tshaka.dev>
2023-06-16 16:13:27 +0200

    feat: validate elliptic curves

--- src/standards/nist.rs
+++ src/standards/nist.rs
@@ -20,6 +20,7 @@ use std::ffi::c_int;
 use lazy_static::lazy_static;
 
 use crate::context::Context;
+use crate::primitives::ecc::*;
 use crate::primitives::ffc::*;
 use crate::primitives::hash::*;
 use crate::primitives::ifc::*;
@@ -29,6 +30,27 @@ const CUTOFF_YEAR: u16 = 2031;
 const CUTOFF_YEAR_3TDEA: u16 = 2023;
 
 lazy_static! {
+  static ref SPECIFIED_EC: HashSet<u16> = {
+    let mut s = HashSet::new();
+    s.insert(P224.id);
+    s.insert(P256.id);
+    s.insert(P384.id);
+    s.insert(P521.id);
+    s.insert(W25519.id);
+    s.insert(W448.id);
+    s.insert(Curve25519.id);
+    s.insert(Curve448.id);
+    s.insert(Edwards25519.id);
+    s.insert(Edwards448.id);
+    s.insert(E448.id);
+    s.insert(brainpoolP224r1.id);
+    s.insert(brainpoolP256r1.id);
+    s.insert(brainpoolP320r1.id);
+    s.insert(brainpoolP384r1.id);
+    s.insert(brainpoolP512r1.id);
+    s.insert(secp256k1.id);
+    s
+  };
   static ref SPECIFIED_HASH: HashSet<u16> = {
     let mut s = HashSet::new();
     s.insert(SHA1.id);
@@ -113,6 +135,56 @@ pub fn validate_ffc(ctx: &Context, key: &Ffc) -> Result<Ffc, Ffc> {
       n: 385..,
     } => Ok(FFC_15360_512),
     _ => Err(FFC_2048_224),
+  }
+}
+
+/// Validate an elliptic curve cryptography primitive used for digital
+/// signatures and key establishment where f is the key size according
+/// to page 54-55 of the standard.
+///
+/// If the key is not compliant then `Err` will contain the recommended
+/// primitive that one should use instead.
+///
+/// If the key is compliant but the context specifies a higher security
+/// level, `Ok` will also hold the recommended primitive with the
+/// desired security level.
+///
+/// # Example
+///
+/// The following illustrates a call to validate a compliant key.
+///
+/// ```
+/// use wardstone::context::Context;
+/// use wardstone::primitives::ecc::P224;
+/// use wardstone::standards::nist;
+///
+/// let ctx = Context::default();
+/// assert_eq!(nist::validate_ecc(&ctx, &P224), Ok(P224));
+/// ```
+pub fn validate_ecc(ctx: &Context, key: &Ecc) -> Result<Ecc, Ecc> {
+  if SPECIFIED_EC.contains(&key.id) {
+    let security = ctx.security().max(key.f >> 1);
+    match security {
+      ..=111 => {
+        if ctx.year() > CUTOFF_YEAR {
+          Err(P256)
+        } else {
+          Err(P224)
+        }
+      },
+      112 => {
+        if ctx.year() > CUTOFF_YEAR {
+          Err(P256)
+        } else {
+          Ok(P224)
+        }
+      },
+      113..=128 => Ok(P256),
+      129..=192 => Ok(P384),
+      193.. => Ok(P521),
+    }
+  } else {
+    Err(P256)
   }
 }
 
@@ -368,6 +440,32 @@ unsafe fn c_call<T>(
   is_compliant as c_int
 }
 
+/// Validate an elliptic curve cryptography primitive used for digital
+/// signatures and key establishment where f is the key size according
+/// to page 54-55 of the standard.
+///
+/// If the key is not compliant then `ws_ecc*` will contain the
+/// recommended primitive that one should use instead.
+///
+/// If the key is compliant but the context specifies a higher security
+/// level, `ws_ecc*` will also hold the recommended primitive with the
+/// desired security level.
+///
+/// The function returns 1 if the key is compliant, 0 if it is not, and
+/// -1 if an error occurs as a result of a missing or invalid argument.
+///
+/// # Safety
+///
+/// See module documentation for comment on safety.
+#[no_mangle]
+pub unsafe extern "C" fn ws_nist_validate_ecc(
+  ctx: *const Context,
+  key: *const Ecc,
+  alternative: *mut Ecc,
+) -> c_int {
+  c_call(validate_ecc, ctx, key, alternative)
+}
+
 /// Validates a finite field cryptography primitive function examples
 /// which include DSA and key establishment algorithms such as
 /// Diffie-Hellman and MQV according to page 54-55 of the standard.
@@ -555,6 +653,24 @@ mod tests {
       }
     };
   }
+
+  test_case!(p224, validate_ecc, &P224, Ok(P224));
+  test_case!(p256, validate_ecc, &P256, Ok(P256));
+  test_case!(p384, validate_ecc, &P384, Ok(P384));
+  test_case!(p521, validate_ecc, &P521, Ok(P521));
+  test_case!(w25519, validate_ecc, &W25519, Ok(P256));
+  test_case!(w448, validate_ecc, &W448, Ok(P521));
+  test_case!(curve25519, validate_ecc, &Curve25519, Ok(P256));
+  test_case!(curve488, validate_ecc, &Curve448, Ok(P521));
+  test_case!(edwards25519, validate_ecc, &Edwards25519, Ok(P256));
+  test_case!(edwards448, validate_ecc, &Edwards448, Ok(P521));
+  test_case!(e448, validate_ecc, &E448, Ok(P521));
+  test_case!(brainpoolp224r1, validate_ecc, &brainpoolP224r1, Ok(P224));
+  test_case!(brainpoolp256r1, validate_ecc, &brainpoolP256r1, Ok(P256));
+  test_case!(brainpoolp320r1, validate_ecc, &brainpoolP320r1, Ok(P384));
+  test_case!(brainpoolp384r1, validate_ecc, &brainpoolP384r1, Ok(P384));
+  test_case!(brainpoolp512r1, validate_ecc, &brainpoolP512r1, Ok(P521));
+  test_case!(secp256k1_, validate_ecc, &secp256k1, Ok(P256));
 
   test_case!(ffc_1024_160, validate_ffc, &FFC_1024_160, Err(FFC_2048_224));
   test_case!(ffc_2048_224, validate_ffc, &FFC_2048_224, Ok(FFC_2048_224));
