ba59a9dd4a5fb2b1ef2355ece9da1cd277c4c619
Tshaka Lekholoane <mail+git@tshaka.dev>
2023-06-22 15:08:41 +0200

    feat: validate FFC primitives

--- src/standards/bsi.rs
+++ src/standards/bsi.rs
@@ -21,6 +21,7 @@ use std::ffi::c_int;
 use lazy_static::lazy_static;
 
 use crate::primitives::ecc::*;
+use crate::primitives::ffc::*;
 use crate::primitives::hash::*;
 use crate::primitives::symmetric::*;
 use crate::standards;
@@ -95,6 +96,60 @@ pub fn validate_ecc(ctx: &Context, key: &Ecc) -> Result<Ecc, Ecc> {
     }
   } else {
     Err(brainpoolP256r1)
+  }
+}
+
+/// Validates a finite field cryptography primitive.
+///
+/// Examples include the DSA and key establishment algorithms such as
+/// Diffie-Hellman.
+///
+/// If the key is not compliant then `Err` will contain the recommended
+/// key sizes L and N that one should use instead.
+///
+/// If the key is compliant but the context specifies a higher security
+/// level, `Ok` will also hold the recommended key sizes L and N with
+/// the desired security level.
+///
+/// **Note:** The choice of security specified in the `Context` is
+/// restricted to the values 160, 224, 256, 384, and 512.
+///
+/// # Example
+///
+/// The following illustrates a call to validate a non-compliant key.
+///
+/// ```
+/// use wardstone::context::Context;
+/// use wardstone::primitives::ffc::{FFC_2048_224, FFC_3072_256};
+/// use wardstone::standards::bsi;
+///
+/// let ctx = Context::default();
+/// let dsa_2048 = FFC_2048_224;
+/// let dsa_3072 = FFC_3072_256;
+/// assert_eq!(bsi::validate_ffc(&ctx, &dsa_2048), Err(dsa_3072));
+/// ```
+pub fn validate_ffc(ctx: &Context, key: &Ffc) -> Result<Ffc, Ffc> {
+  // HACK: Use the public key size n as a proxy for security.
+  let mut aux = *key;
+  aux.n = ctx.security().max(key.n);
+  match aux {
+    Ffc {
+      l: ..=2999,
+      n: ..=249,
+    } => Err(FFC_3072_256),
+    Ffc {
+      l: 3000..=3072,
+      n: 250..=256,
+    } => Ok(FFC_3072_256),
+    Ffc {
+      l: 3073..=7680,
+      n: 257..=384,
+    } => Ok(FFC_7680_384),
+    Ffc {
+      l: 7681..,
+      n: 385..,
+    } => Ok(FFC_15360_512),
+    _ => Err(NOT_SUPPORTED),
   }
 }
 
@@ -274,6 +329,37 @@ pub unsafe extern "C" fn ws_bsi_validate_ecc(
   standards::c_call(validate_ecc, ctx, key, alternative)
 }
 
+/// Validates a finite field cryptography primitive.
+///
+/// Examples include the DSA and key establishment algorithms such as
+/// Diffie-Hellman.
+///
+/// If the key is not compliant then `struct ws_ffc*` will point to the
+/// recommended primitive that one should use instead.
+///
+/// If the key is compliant but the context specifies a higher security
+/// level, `struct ws_ffc` will also point to the recommended primitive
+/// with the desired security level.
+///
+/// The function returns `1` if the hash function is compliant, `0` if
+/// it is not, and `-1` if an error occurs as a result of a missing or
+/// invalid argument.
+///
+/// **Note:** The choice of security specified in the `Context` is
+/// restricted to the values 160, 224, 256, 384, and 512.
+///
+/// # Safety
+///
+/// See module documentation for comment on safety.
+#[no_mangle]
+pub unsafe extern "C" fn ws_bsi_validate_ffc(
+  ctx: *const Context,
+  key: *const Ffc,
+  alternative: *mut Ffc,
+) -> c_int {
+  standards::c_call(validate_ffc, ctx, key, alternative)
+}
+
 /// Validates a hash function according to page 41 of the guide. The
 /// reference is made with regards to applications that require
 /// collision resistance such as digital signatures.
@@ -414,6 +500,12 @@ mod tests {
   test_case!(brainpoolp384r1, validate_ecc, &brainpoolP384r1, Ok(brainpoolP384r1));
   test_case!(brainpoolp512r1, validate_ecc, &brainpoolP512r1, Ok(brainpoolP512r1));
   test_case!(secp256k1_, validate_ecc, &secp256k1, Err(brainpoolP256r1));
+
+  test_case!(ffc_1024_160, validate_ffc, &FFC_1024_160, Err(FFC_3072_256));
+  test_case!(ffc_2048_224, validate_ffc, &FFC_2048_224, Err(FFC_3072_256));
+  test_case!(ffc_3072_256, validate_ffc, &FFC_3072_256, Ok(FFC_3072_256));
+  test_case!(ffc_7680_384, validate_ffc, &FFC_7680_384, Ok(FFC_7680_384));
+  test_case!(ffc_15360_512, validate_ffc, &FFC_15360_512, Ok(FFC_15360_512));
 
   test_case!(blake2b_256_collision_resistance, validate_hash, &BLAKE2b_256, Err(SHA256));
   test_case!(blake2b_384_collision_resistance, validate_hash, &BLAKE2b_384, Err(SHA256));
