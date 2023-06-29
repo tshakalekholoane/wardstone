fcf893c1127f3d4f979170cc9ebd96486f8b248d
Tshaka Lekholoane <mail+git@tshaka.dev>
2023-06-29 16:39:00 +0200

    feat: update symmetric validation for Lenstra module

--- src/standards/lenstra.rs
+++ src/standards/lenstra.rs
@@ -16,11 +16,11 @@
 use std::collections::HashSet;
 use std::ffi::c_int;
 
-use crate::primitives::symmetric::Symmetric;
 use lazy_static::lazy_static;
 
 use crate::context::Context;
 use crate::primitives::hash::*;
+use crate::primitives::symmetric::*;
 use crate::standards;
 
 #[derive(PartialEq, Eq, Debug)]
@@ -32,7 +32,6 @@ const BASE_YEAR: u16 = 1982;
 const BASE_SECURITY: u16 = 56;
 
 lazy_static! {
-  // Not strict. See p. 13 and discussion therein.
   static ref SPECIFIED_HASH: HashSet<u16> = {
     let mut s = HashSet::new();
     s.insert(RIPEMD160.id);
@@ -40,6 +39,18 @@ lazy_static! {
     s.insert(SHA256.id);
     s.insert(SHA384.id);
     s.insert(SHA512.id);
+    s
+  };
+  static ref SPECIFIED_SYMMETRIC: HashSet<u16> = {
+    let mut s = HashSet::new();
+    s.insert(AES128.id);
+    s.insert(AES192.id);
+    s.insert(AES256.id);
+    s.insert(DES.id);
+    s.insert(DESX.id);
+    s.insert(IDEA.id);
+    s.insert(TDEA2.id);
+    s.insert(TDEA3.id);
     s
   };
 }
@@ -114,10 +125,49 @@ pub fn validate_hash(ctx: &Context, hash: &Hash) -> Result<Hash, Hash> {
   }
 }
 
-/// Validates a symmetric key primitive according to pages 11-12 of the
+/// Validates a symmetric key primitive according to pages 9-12 of the
 /// paper.
-pub fn validate_symmetric(symmetric: &Symmetric, expiry: u16) -> Result<bool> {
-  calculate_year(symmetric.security).map(|year| year >= expiry)
+///
+/// If the key is not compliant then `Err` will contain the recommended
+/// primitive that one should use instead.
+///
+/// If the key is compliant but the context specifies a higher security
+/// level, `Ok` will also hold the recommended primitive with the
+/// desired security level.
+///
+/// # Example
+///
+/// The following illustrates a call to validate a compliant key.
+///
+/// ```
+/// use wardstone::context::Context;
+/// use wardstone::primitives::symmetric::TDEA3;
+/// use wardstone::standards::lenstra;
+///
+/// let ctx = Context::default();
+/// assert_eq!(lenstra::validate_symmetric(&ctx, &TDEA3), Ok(TDEA3));
+/// ```
+pub fn validate_symmetric(ctx: &Context, key: &Symmetric) -> Result<Symmetric, Symmetric> {
+  if SPECIFIED_SYMMETRIC.contains(&key.id) {
+    let implied_security = ctx.security().max(key.security);
+    let recommendation = match implied_security {
+      ..=95 => TDEA2,
+      96..=112 => TDEA3,
+      113..=120 => DESX,
+      121..=128 => AES128,
+      129..=192 => AES192,
+      193.. => AES256,
+    };
+    calculate_security(ctx.year()).map_or(Err(recommendation), |minimum_security| {
+      if implied_security < minimum_security {
+        Err(recommendation)
+      } else {
+        Ok(recommendation)
+      }
+    })
+  } else {
+    Err(AES128)
+  }
 }
 
 /// Validates a hash function according to page 14 of the paper.
@@ -153,23 +203,32 @@ pub unsafe extern "C" fn ws_lenstra_validate_hash(
   standards::c_call(validate_hash, ctx, hash, alternative)
 }
 
-/// Validates a symmetric key primitive according to pages 11-12 of the
+/// Validates a symmetric key primitive according to pages 9-12 of the
 /// paper.
+///
+/// If the key is not compliant then `struct ws_symmetric* alternative`
+/// will point to the recommended primitive that one should use instead.
+///
+/// If the key is compliant but the context specifies a higher security
+/// level, `struct ws_symmetric*` will also point to the recommended
+/// primitive with the desired security level.
+///
+/// The function returns `1` if the hash function is compliant, `0` if
+/// it is not, and `-1` if an error occurs as a result of a missing or
+/// invalid argument.
 ///
 /// # Safety
 ///
-/// See [module documentation](crate::standards::lenstra) for comment on
-/// safety.
+/// See module documentation for comment on safety.
 #[no_mangle]
-pub unsafe extern "C" fn lenstra_validate_symmetric(
-  symmetric: *const Symmetric,
-  expiry: u16,
+pub unsafe extern "C" fn ws_lenstra_validate_symmetric(
+  ctx: *const Context,
+  key: *const Symmetric,
+  alternative: *mut Symmetric,
 ) -> c_int {
-  if let Some(symmetric_ref) = unsafe { symmetric.as_ref() } {
-    validate_symmetric(symmetric_ref, expiry).map_or(-1, |is_compliant| is_compliant as c_int)
-  } else {
-    -1
-  }
+  standards::c_call(validate_symmetric, ctx, key, alternative)
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
@@ -200,4 +259,19 @@ mod tests {
   test_case!(shake128, validate_hash, &SHAKE128, Err(SHA256));
   test_case!(shake256, validate_hash, &SHAKE256, Err(SHA256));
   test_case!(whirlpool, validate_hash, &WHIRLPOOL, Err(SHA256));
+
+  test_case!(aes128, validate_symmetric, &AES128, Ok(AES128));
+  test_case!(aes192, validate_symmetric, &AES192, Ok(AES192));
+  test_case!(aes256, validate_symmetric, &AES256, Ok(AES256));
+  test_case!(camellia128, validate_symmetric, &Camellia128, Err(AES128));
+  test_case!(camellia192, validate_symmetric, &Camellia192, Err(AES128));
+  test_case!(camellia256, validate_symmetric, &Camellia256, Err(AES128));
+  test_case!(des, validate_symmetric, &DES, Err(TDEA2));
+  test_case!(desx, validate_symmetric, &DESX, Ok(DESX));
+  test_case!(idea, validate_symmetric, &IDEA, Ok(AES128));
+  test_case!(serpent128, validate_symmetric, &Serpent128, Err(AES128));
+  test_case!(serpent192, validate_symmetric, &Serpent192, Err(AES128));
+  test_case!(serpent256, validate_symmetric, &Serpent256, Err(AES128));
+  test_case!(three_key_tdea, validate_symmetric, &TDEA3, Ok(TDEA3));
+  test_case!(two_key_tdea, validate_symmetric, &TDEA2, Ok(TDEA2));
 }
