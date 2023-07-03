509a2122a194f89697a76f285342daf036bd1055
Tshaka Lekholoane <mail+git@tshaka.dev>
2023-07-03 13:48:25 +0200

    feat: add more IFC instances
    Particularly those in Lenstra (2004, p. 25).

--- src/primitives/ifc.rs
+++ src/primitives/ifc.rs
@@ -12,6 +12,16 @@ pub struct Ifc {
 #[no_mangle]
 pub static IFC_1024: Ifc = Ifc { k: 1024 };
 
+/// Generic instance that represents a choice of k = 1280 for an integer
+/// factorisation cryptography primitive.
+#[no_mangle]
+pub static IFC_1280: Ifc = Ifc { k: 1280 };
+
+/// Generic instance that represents a choice of k = 1536 for an integer
+/// factorisation cryptography primitive.
+#[no_mangle]
+pub static IFC_1536: Ifc = Ifc { k: 1536 };
+
 /// Generic instance that represents a choice of k = 2048 for an integer
 /// factorisation cryptography primitive.
 #[no_mangle]
@@ -22,10 +32,20 @@ pub static IFC_2048: Ifc = Ifc { k: 2048 };
 #[no_mangle]
 pub static IFC_3072: Ifc = Ifc { k: 3072 };
 
+/// Generic instance that represents a choice of k = 4096 for an integer
+/// factorisation cryptography primitive.
+#[no_mangle]
+pub static IFC_4096: Ifc = Ifc { k: 4096 };
+
 /// Generic instance that represents a choice of k = 7680 for an integer
 /// factorisation cryptography primitive.
 #[no_mangle]
 pub static IFC_7680: Ifc = Ifc { k: 7680 };
+
+/// Generic instance that represents a choice of k = 8192 for an integer
+/// factorisation cryptography primitive.
+#[no_mangle]
+pub static IFC_8192: Ifc = Ifc { k: 8192 };
 
 /// Generic instance that represents a choice of k = 15360 for an
 /// integer factorisation cryptography primitive.
