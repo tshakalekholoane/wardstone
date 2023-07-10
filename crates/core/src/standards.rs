5a4ed0c6c6f1944e38479b2a7fa7588b6b8df01b
Tshaka Lekholoane <mail+git@tshaka.dev>
2023-07-10 15:49:31 +0200

    refactor: move `test_case` utility macro
    This also hides the module in the public documentation.

--- crates/core/src/standards.rs
+++ crates/core/src/standards.rs
@@ -5,3 +5,17 @@ pub mod cnsa;
 pub mod ecrypt;
 pub mod lenstra;
 pub mod nist;
+
+/// Internal macro used to reduce verbosity in unit tests.
+#[doc(hidden)]
+#[macro_export]
+macro_rules! test_case {
+  ($name:ident, $func:ident, $input:expr, $want:expr) => {
+    #[test]
+    fn $name() {
+      use $crate::context::Context;
+      let ctx = Context::default();
+      assert_eq!($func(&ctx, $input), $want);
+    }
+  };
+}
