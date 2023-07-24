e3335928adc75f11375950991126f928d7543ace
Tshaka Lekholoane <mail+git@tshaka.dev>
2023-07-24 14:46:33 +0200

    feat: validate X.509 certificates

--- crates/cmd/src/main.rs
+++ crates/cmd/src/main.rs
@@ -1,6 +1,7 @@
 use std::path::PathBuf;
 
-use clap::{Parser, Subcommand, ValueEnum};
+use clap::{Parser, Subcommand};
+use wardstone::assess::{self, Guide};
 
 /// Assess cryptographic keys for compliance.
 #[derive(Parser)]
@@ -10,33 +11,22 @@ struct Options {
   subcommands: Subcommands,
 }
 
-#[derive(Clone, Copy, Debug, ValueEnum)]
-enum Guide {
-  /// The BSI TR-02102 series of technical guidelines.
-  Bsi,
-}
-
 #[derive(Subcommand)]
 enum Subcommands {
   /// Check an X.509 public key certificate for compliance.
   X509 {
+    /// The certificate as a DER or PEM encoded file.
+    #[arg(short, long, value_name = "FILE")]
+    path: PathBuf,
     /// Guide to assess the certificate against.
     #[arg(short, long, value_enum)]
     guide: Guide,
-    /// The certificate as a DER or PEM encoded file.
-    #[arg(short, long, value_name = "FILE")]
-    certificate: PathBuf,
   },
 }
 
 fn main() {
   let options = Options::parse();
   match &options.subcommands {
-    Subcommands::X509 { guide, certificate } => {
-      println!(
-        "debug: assess the X.509 certificate at {:?} for {:?} compliance",
-        certificate, guide
-      );
-    },
+    Subcommands::X509 { guide, path } => assess::x509(path, guide),
   }
 }
