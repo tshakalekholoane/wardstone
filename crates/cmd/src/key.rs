42feaa69bbaa1cabdd057204bd773f2cb0988ed6
Tshaka Lekholoane <mail+git@tshaka.dev>
2023-08-04 16:33:42 +0200

    refactor: simplify structure

--- crates/cmd/src/key.rs
+++ crates/cmd/src/key.rs
@@ -1,33 +1,92 @@
+use std::fs::File;
+use std::io::Read;
+use std::path::PathBuf;
 use std::{fmt, io};
 
-pub mod certificate;
+use openssl::pkey::Id;
+use openssl::x509::X509;
+use wardstone_core::primitive::hash::Hash;
+
+use crate::bridge::{Asymmetric, HASH_FUNCTIONS, SIGNATURE_ALGORITHMS};
+
+/// Represents a TLS certificate.
+#[derive(Debug)]
+pub struct Certificate(X509);
+
+impl Certificate {
+  pub fn extract_hash_function(&self) -> Option<&Hash> {
+    let name = self
+      .0
+      .signature_algorithm()
+      .object()
+      .nid()
+      .signature_algorithms()?
+      .digest
+      .long_name()
+      .ok()?;
+    HASH_FUNCTIONS.get(name).map(|hash| *hash)
+  }
+
+  pub fn extract_signature_algorithm(&self) -> Option<&Asymmetric> {
+    let public_key = self.0.public_key().ok()?;
+    match public_key.id() {
+      Id::DH
+      | Id::DSA
+      | Id::EC
+      | Id::RSA
+      | Id::ED25519
+      | Id::ED448
+      | Id::SM2
+      | Id::X25519
+      | Id::X448 => {
+        let name = public_key
+          .ec_key()
+          .ok()?
+          .group()
+          .curve_name()?
+          .long_name()
+          .ok()?;
+        SIGNATURE_ALGORITHMS.get(name)
+      },
+      _ => None,
+    }
+  }
+
+  pub fn from_pem_file(path: &PathBuf) -> Result<Certificate, Error> {
+    let mut file = File::open(path)?;
+    let mut contents = Vec::new();
+    file.read_to_end(&mut contents)?;
+    let certificate = X509::from_pem(&contents)?;
+    Ok(Self(certificate))
+  }
+}
 
 #[derive(Debug)]
-pub enum KeyError {
-  Io(std::io::Error),
+pub enum Error {
+  Io(io::Error),
   Parse(openssl::error::ErrorStack),
 }
 
-impl fmt::Display for KeyError {
+impl fmt::Display for Error {
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
     match self {
-      KeyError::Io(err) => match err.kind() {
+      Error::Io(err) => match err.kind() {
         io::ErrorKind::NotFound => write!(f, "Key not found."),
         io::ErrorKind::PermissionDenied => write!(f, "Permission denied."),
         _ => write!(f, "Unexpected error. Please file an issue."),
       },
-      KeyError::Parse(_) => write!(f, "Key parse error."),
+      Error::Parse(_) => write!(f, "Key parse error."),
     }
   }
 }
 
-impl From<io::Error> for KeyError {
+impl From<io::Error> for Error {
   fn from(err: io::Error) -> Self {
     Self::Io(err)
   }
 }
 
-impl From<openssl::error::ErrorStack> for KeyError {
+impl From<openssl::error::ErrorStack> for Error {
   fn from(err: openssl::error::ErrorStack) -> Self {
     Self::Parse(err)
   }
--- crates/cmd/src/main.rs
+++ crates/cmd/src/main.rs
@@ -1,8 +1,93 @@
+use std::fmt;
 use std::path::PathBuf;
+use std::process::{ExitCode, Termination};
 
-use clap::{Parser, Subcommand};
-use wardstone::assess::{self, Guide, Status};
+use clap::{Parser, Subcommand, ValueEnum};
+use wardstone::bridge::Asymmetric;
+use wardstone::key::Certificate;
 use wardstone_core::context::Context;
+use wardstone_core::primitive::hash::*;
+use wardstone_core::standard::bsi::Bsi;
+use wardstone_core::standard::cnsa::Cnsa;
+use wardstone_core::standard::Standard;
+
+pub enum Status {
+  Ok(PathBuf),
+  Fail(PathBuf),
+}
+
+impl Termination for Status {
+  fn report(self) -> std::process::ExitCode {
+    match self {
+      Self::Ok(_) => {
+        println!("{}", &self);
+        ExitCode::SUCCESS
+      },
+      Self::Fail(_) => {
+        eprintln!("{}", &self);
+        ExitCode::FAILURE
+      },
+    }
+  }
+}
+
+impl fmt::Display for Status {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    match &self {
+      Self::Ok(path) => write!(f, "ok: {}", path.display()),
+      Self::Fail(path) => write!(f, "fail: {}", path.display()),
+    }
+  }
+}
+
+#[derive(Clone, Copy, Debug, ValueEnum)]
+pub enum Guide {
+  /// The BSI TR-02102 series of technical guidelines.
+  Bsi,
+  Cnsa,
+}
+
+impl Guide {
+  fn validate_hash_function(
+    &self,
+    ctx: &Context,
+    hash: &Hash,
+  ) -> Result<&'static Hash, &'static Hash> {
+    match self {
+      Self::Bsi => Bsi::validate_hash(ctx, hash),
+      Self::Cnsa => Cnsa::validate_hash(ctx, hash),
+    }
+  }
+
+  fn validate_signature_algorithm(
+    &self,
+    ctx: &Context,
+    algorithm: &Asymmetric,
+  ) -> Result<Asymmetric, Asymmetric> {
+    match self {
+      Self::Bsi => match algorithm {
+        Asymmetric::Ecc(instance) => match Bsi::validate_ecc(ctx, instance) {
+          Ok(instance) => Ok(Asymmetric::Ecc(instance)),
+          Err(instance) => Err(Asymmetric::Ecc(instance)),
+        },
+        Asymmetric::Ifc(instance) => match Bsi::validate_ifc(ctx, instance) {
+          Ok(instance) => Ok(Asymmetric::Ifc(instance)),
+          Err(instance) => Err(Asymmetric::Ifc(instance)),
+        },
+      },
+      Self::Cnsa => match algorithm {
+        Asymmetric::Ecc(instance) => match Cnsa::validate_ecc(ctx, instance) {
+          Ok(instance) => Ok(Asymmetric::Ecc(instance)),
+          Err(instance) => Err(Asymmetric::Ecc(instance)),
+        },
+        Asymmetric::Ifc(instance) => match Cnsa::validate_ifc(ctx, instance) {
+          Ok(instance) => Ok(Asymmetric::Ifc(instance)),
+          Err(instance) => Err(Asymmetric::Ifc(instance)),
+        },
+      },
+    }
+  }
+}
 
 /// Assess cryptographic keys for compliance.
 #[derive(Parser)]
@@ -29,13 +114,55 @@ enum Subcommands {
 }
 
 impl Subcommands {
+  fn x509(ctx: &Context, path: &PathBuf, guide: &Guide, verbose: &bool) -> Status {
+    let certificate = match Certificate::from_pem_file(path) {
+      Ok(got) => got,
+      Err(err) => {
+        eprintln!("{}", err.to_string());
+        return Status::Fail(path.to_path_buf());
+      },
+    };
+
+    let mut pass = Status::Ok(path.to_path_buf());
+
+    if let Some(got) = certificate.extract_hash_function() {
+      match guide.validate_hash_function(ctx, got) {
+        Ok(want) => {
+          if *verbose {
+            println!("hash function: got: {}, want: {}", got, want)
+          }
+        },
+        Err(want) => {
+          pass = Status::Fail(path.to_path_buf());
+          eprintln!("hash function: got: {}, want: {}", got, want);
+        },
+      }
+    }
+
+    if let Some(got) = certificate.extract_signature_algorithm() {
+      match guide.validate_signature_algorithm(ctx, got) {
+        Ok(want) => {
+          if *verbose {
+            println!("signature algorithm: got: {}, want: {}", got, want)
+          }
+        },
+        Err(want) => {
+          pass = Status::Fail(path.to_path_buf());
+          eprintln!("signature algorithm: got: {}, want: {}", got, want);
+        },
+      }
+    }
+
+    pass
+  }
+
   pub fn run(&self, &ctx: &Context) -> Status {
     match self {
       Self::X509 {
         guide,
         path,
         verbose,
-      } => assess::x509(&ctx, path, guide, verbose),
+      } => Self::x509(&ctx, path, guide, verbose),
     }
   }
 }
